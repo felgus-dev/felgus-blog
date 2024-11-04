c1ac819b9fdd3f5d7cf876ec7c267ca8c60669dd
Felipe Gustavo <felipegdas07@gmail.com>
2024-11-04 18:04:56 -0300

    feat(posts): add async derivations post and update on main description

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Page() {
         Felipe Gustavo's blog
       </h1>
       <p className="mb-4">
-        {`Hey, I’m Felipe, a React enthusiast and frontend engineer, 
-      specialized in building high-quality user interfaces and frontend web 
-      applications. With great interest in javascript ecosystem, state management libraries, 
+        {`Hey, I’m Felipe, a Frontend engineer and React enthusiast, based in São Paulo, Brazil. 
+      Specialized in building high-quality user interfaces and frontend web 
+      applications. With high interest in javascript ecosystem, state management libraries, 
       fine-grained reactivity, tech products solutions and API design, 
       I will share here ideas and experiences about web and technology in general.`}
       </p>
