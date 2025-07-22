591c86f9af1d86dc1e98bbd9583678f8d363b9c5
Felipe Gustavo <felipegdas07@gmail.com>
2025-07-22 18:45:28 -0300

    fix(analytics): change analytics to plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import { Navbar } from "./components/nav";
-import { GoogleAnalytics } from '@next/third-parties/google';
 import Footer from "./components/footer";
 import { baseUrl } from "./sitemap";
+import PlausibleProvider from 'next-plausible'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -52,14 +52,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cx(GeistSans.variable, GeistMono.variable)}>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <GoogleAnalytics gaId="G-LK133X7M5G" />
-        </main>
-      </body>
+      <PlausibleProvider domain="felgus.dev">
+        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+          <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+            <Navbar />
+            {children}
+            <Footer />
+          </main>
+        </body>
+      </PlausibleProvider>
     </html>
   );
 }
